33e09baf502d4e9c2ddaae5b960873f3a0c812fb
Joe Heck <heckj@mac.com>
2016-07-19 11:25:22 -0700

    updating copyright

--- docs/conf.py
+++ docs/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'RackHD'
-copyright = u'2015, EMC'
+copyright = u'2015-2016, EMC'
 author = u'EMC'
 
 # The version info for the project you're documenting, acts as replacement for
